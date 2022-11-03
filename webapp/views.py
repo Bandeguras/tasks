789dc914012ecb6677c4ebb79300e21fd107ce68
Said Saliev <repsaid@gmail.com>
2022-11-03 21:58:25 +0600

    add page for creating new tasks

--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from webapp.models import Task
+from webapp.models import Task, STATUS_CHOICES
 # Create your views here.
 
 
@@ -9,3 +9,16 @@ def index_view(request):
         'tasks': tasks,
     }
     return render(request, 'index.html', context)
+
+def create_task_view(request):
+    if request.method == 'GET':
+        return render(request, "create_task.html", {'statuses': STATUS_CHOICES})
+    elif request.method == 'POST':
+        description = request.POST.get('description')
+        status = request.POST.get('status')
+        completion_at = request.POST.get('completion_at')
+        if completion_at == '':
+            completion_at = None
+        new_task = Task.objects.create(description=description, status=status, completion_at=completion_at)
+        context = {'task': new_task}
+        return render(request, 'task_view.html', context)
