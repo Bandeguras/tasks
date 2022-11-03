8afd7d6ebd5b676a95566e2c8c528056f975de86
Said Saliev <repsaid@gmail.com>
2022-11-03 20:33:15 +0600

    add database and make migration

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+STATUS_CHOICES = [('new', 'Новая'), ('in_progress', 'В процессе'),  ('done', 'Сделано')]
+class Task(models.Model):
+    description = models.CharField(max_length=50, null=False, blank=False, verbose_name="Описание")
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=STATUS_CHOICES[0][0], verbose_name="СТАТУС")
+    completion_at = models.DateField(max_length=20, null=True, blank=True, verbose_name="Дата выполнения")
+
