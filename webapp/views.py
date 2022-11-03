e63e28190053d25428908a2cfc019247a948678a
Said Saliev <repsaid@gmail.com>
2022-11-03 20:48:25 +0600

    add main page

--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from webapp.models import Task
 # Create your views here.
+
+
+def index_view(request):
+    tasks = Task.objects.order_by('-completion_at')
+    context = {
+        'tasks': tasks,
+    }
+    return render(request, 'index.html', context)
